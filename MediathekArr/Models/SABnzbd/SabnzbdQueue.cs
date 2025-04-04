84f621c13a9b4f2fb2c3e394f72866a2c07a06a7
MrAn0nym <63542658+MrAn0nym@users.noreply.github.com>
2025-04-04 09:34:21 +0000

    Add support for the Homarr SABnzbd integration …
    … by mocking the translate endpoint
    … by returning 0 for `kbpersec` in the `/queue` response
    … by returning the `completed` timestamp (working as intended) and 0 for `postproc_time` in the `/history` response

--- MediathekArr/Models/SABnzbd/SabnzbdQueue.cs
+++ MediathekArr/Models/SABnzbd/SabnzbdQueue.cs
@@ -7,6 +7,9 @@ public class SabnzbdQueue
     [JsonPropertyName("paused")]
     public bool Paused => false;
 
+    [JsonPropertyName("kbpersec")]
+    public string KbPerSec => "0";
+
     [JsonPropertyName("slots")]
     public List<SabnzbdQueueItem> Items { get; set; }
 }
